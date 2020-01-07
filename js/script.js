62a63e5a6bae595f3b8d5709c45154e9d0803375
wesleykuang12 <wesleykuang12@gmail.com>
2020-01-07 02:22:23 +0000

    playlist stuff

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let songList = ["Bye Bye Bye", "Lose Yourself", "Break Your Heart", "The Man", "Secrets"]
+
+songList.forEach(function(song) {
+    $('.songList').append("<h4>" + song + "</h4>")
+});
+
+
+
+let songList = ["NSYNC", "Em", "Break Your Heart", "The Man", "Secrets"]
+
+songList.forEach(function(song) {
+    $('.songList').append("<h4>" + song + "</h4>")
+});
+
+
+
+let songList = ["Bye Bye Bye", "Lose Yourself", "Break Your Heart", "The Man", "Secrets"]
+
+songList.forEach(function(song) {
+    $('.songList').append("<h4>" + song + "</h4>")
+});
+
+
+
+let songList = ["Bye Bye Bye", "Lose Yourself", "Break Your Heart", "The Man", "Secrets"]
+
+songList.forEach(function(song) {
+    $('.songList').append("<h4>" + song + "</h4>")
+});
+
+
+
+let songList = ["Bye Bye Bye", "Lose Yourself", "Break Your Heart", "The Man", "Secrets"]
+
+songList.forEach(function(song) {
+    $('.songList').append("<h4>" + song + "</h4>")
+});
